3a097ec0336a9028fb1a4bcc08ccdb6a770d96c6
Nickeron-dev@users <75437052+Nickeron-dev@users.noreply.github.com>
2020-12-09 12:17:26 +0200

    First attempt to solve array problem
    failed

--- chapter13/GenDynamicQueue.java
+++ chapter13/GenDynamicQueue.java
@@ -3,16 +3,21 @@ package generalization;
 public class GenDynamicQueue<T> implements IGenQ<T> {
   private T[] queue;
   private int putloc, getloc;
+  private T[] newQueue;
 
   public GenDynamicQueue(T[] array) {
+    //Class typeOfArray
     queue = array;
     putloc = getloc = 0;
+    //newQueue = array(length + 1)
   }
 
   public void put(T obj) {
     if(putloc == queue.length) {
-      T[] newQueue = new T[queue.length+1];  // generic array creation error
-      for(int i = 0; i < queue.length; i++) { 
+      //T<?>[] newQueue = new T<?>[queue.length+1];  // generic array creation error
+      //newQueue = creatingNewArray();
+
+      for(int i = 0; i < queue.length; i++) {
         newQueue[i] = queue[i];
       }
       queue = newQueue;
@@ -29,4 +34,13 @@ public class GenDynamicQueue<T> implements IGenQ<T> {
 
     return queue[getloc++];
   }
+
+  //private Integer[] creatingNewArray(int lastLength) {
+  //  Integer[] array;
+  //  if(queue.getClass().getComponentType() == "Integer") {
+  //    array = new Integer[queue.length+1];
+//    }
+    //queue.getClass().getComponentType() array = new queue.getClass().getComponentType()[queue.length+1];
+  //  return array;
+//  }
 }
