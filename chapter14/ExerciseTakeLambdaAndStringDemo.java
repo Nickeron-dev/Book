edeb9496d7e534922048be713d0b2615a567d73d
Nickeron-dev@users <75437052+Nickeron-dev@users.noreply.github.com>
2020-12-13 17:10:32 +0200

    Fixed one mistake and ended the exercise.
    I completed same task but with my variant(switch) and added . to !.  In book they solved this with using 'if' and only changed case of characters. Strange is that they typed that they changed spaces to -, too but didn't commit this. So I did this instead of them)

--- chapter14/ExerciseTakeLambdaAndStringDemo.java
+++ chapter14/ExerciseTakeLambdaAndStringDemo.java
@@ -8,21 +8,34 @@ public class ExerciseTakeLambdaAndStringDemo {
   }
 
   public static void main(String[] args) {
-    String instr = "Lambda expressions make java larger";
+    String instr = "Lambda Expressions Make Java Larger.";
     String outstr;
     PrintWriter pw = new PrintWriter(System.out, true);
 
-    TakeLambdaAndString reverse = (str) -> {  // reversing given string
+    // Not to call method at the end I just called it here and typed lambda here
+    outstr = returnString((str) -> {  // reversing given string
       String answer = "";
 
-      for(int i = str.length()-1; i > 0; i--)
-        answer += str.charAt(i);
-
+      for(int i = str.length()-1; i >= 0; i--) {
+        switch(str.charAt(i)) {
+          case ' ':  // changing space to -
+            answer += '-';
+            break;
+          case '.':  // changing . to !
+            answer += '!';
+            break;
+          default:
+            if(Character.isUpperCase(str.charAt(i)))  // turning char to opposite
+              answer += Character.toLowerCase(str.charAt(i)); // case
+            else
+              answer += Character.toUpperCase(str.charAt(i));
+        }
+      }
       return answer;
-    };
+    }, instr);
 
     pw.println("First string is: " + instr);
-    outstr = returnString(reverse, instr);
+    //outstr = returnString(reverse, instr);
     pw.println("Return is: " + outstr); // demonstration
   }
 }
