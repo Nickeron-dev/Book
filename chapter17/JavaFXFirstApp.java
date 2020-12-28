d73e5d3ba2610348aaaf2e2bd72bdd981713a86a
Nickeron-dev <koshkin061969@gmail.com>
2020-12-28 21:01:44 +0200

    I started chapter 17 because it is something strange in 15th. But since 11th java JavaFX doesn't include jdk.

---
+++ chapter17/JavaFXFirstApp.java
@@ -0,0 +1,38 @@
+import java.javafx.application.*;
+import java.javafx.scene.*;
+import java.javafx.stage.*;
+import java.javafx.scene.layout.*;
+
+public class JavaFXFirstApp extends Application {
+	public static void main(String[] args) {
+		System.out.println("This is my first JavaFX app");
+		
+		// lauching file with args
+		launch(args);
+	}
+	
+	public void init() {
+		System.out.println("Now in init()");
+	}
+
+	public void start(Stage myStage) {
+		System.out.println("In start() method");
+
+		myStage.setTitle("My first JavaFX app");
+
+		FlowPane rootNode = new FlowPane();
+
+		// creating a scene
+		Scene myScene = new Scene(rootNode, 300, 200);
+
+		//setting scene on stage
+		myStage.setScene(myScene);
+
+		myStage.show();
+	}
+
+	public void stop() {
+		System.out.println("Now in stop() method");
+	}
+}
+
