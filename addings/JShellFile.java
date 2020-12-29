7aae31bf66ffa411382e105dd6b2d60287e5cce2
Nickeron-dev <koshkin061969@gmail.com>
2020-12-29 22:50:35 +0200

    There is a JShell file

---
+++ addings/JShellFile.java
@@ -0,0 +1,12 @@
+int var1 = 1;
+int var2 =2;
+int var3 = 3;
+void method() {
+pw.println("Something");
+}
+PrintWriter pw = new PrintWriter(System.out, true);
+method()
+"Something" + var1
+23
+1
+$7
