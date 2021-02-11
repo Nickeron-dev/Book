6a54a8845b35128b8d85be52e7eae8a158d07be1
Nickeron-dev <koshkin061969@gmail.com>
2021-02-11 13:40:42 +0200

    Figured out how to use assert. It isn't in my book but I heard about unit tests in a video so I searched it.

---
+++ completeReference/chapter19/MainClass.java
@@ -0,0 +1,22 @@
+package classes;
+
+// this class is very simple just to try testing in Java
+
+// java classes.MainClass // to just run
+// java -ea classes.MainClass // to run with enabled asserts
+
+//import static org.junit.Assert.*;
+//import org.junit.Text;
+
+public class MainClass {
+  //@Test
+  public static void main(String[] args) {
+    assert (2 != divide(6, 3));
+    //assertEquals(2, divide(6, 3));
+    System.out.println("Hello");
+  }
+
+  public static int divide(int a, int b) {
+    return a / b; // just divide
+  }
+}
