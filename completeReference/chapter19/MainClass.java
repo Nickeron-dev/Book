5194bc23f9fa70137ebc4ec8b1892902f89b6372
Nickeron-dev <koshkin061969@gmail.com>
2021-02-11 17:42:40 +0200

    I figured out how to install JUnit on intellij and on linux. Than run both of them.

--- completeReference/chapter19/MainClass.java
+++ completeReference/chapter19/MainClass.java
@@ -1,18 +1,18 @@
-package classes;
+//package classes;
 
 // this class is very simple just to try testing in Java
 
 // java classes.MainClass // to just run
 // java -ea classes.MainClass // to run with enabled asserts
 
-//import static org.junit.Assert.*;
-//import org.junit.Text;
+import static org.junit.Assert.*;
+//mport org.junit.Text;
 
 public class MainClass {
   //@Test
   public static void main(String[] args) {
     assert (2 != divide(6, 3));
-    //assertEquals(2, divide(6, 3));
+    assertEquals(0, divide(6, 3));
     System.out.println("Hello");
   }
 
