f21ee27aada08c7149eca10323d823fcfb39e20f
Nickeron-dev <koshkin061969@gmail.com>
2021-06-26 11:13:53 +0300

    Compiled and launched java application from different directories.

--- course/tooLargeNumber/Number/src/Try.java
+++ course/tooLargeNumber/Number/src/Try.java
@@ -1,7 +1,7 @@
 public class Try {
     public static void main(String[] args) {
-        int number = 2147483648;
-        number += 1;
+        int number = -2147483648;
+        number -= 1;
         System.out.println("Hello " + number);
 	}
 }
