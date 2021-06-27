b9f9d5a98d501058233aa5c3ccf2ab9b47951046
Nickeron-dev <koshkin061969@gmail.com>
2021-06-27 23:48:52 +0300

    Understood some moments about comments.

---
+++ course/lesson1/Comments.java
@@ -0,0 +1,14 @@
+public class Comments {
+  public static void main(String[] args) {
+    // There will be an Error!!!
+    /*
+      2. comment
+      3. /*             // this one it will ignore
+      4. more comments
+      5. */
+//      6. finish
+//      7. */
+
+
+  }
+}
